7444464d45b2836b9f65990e15d97ee977564f33
Mark Carson <markcarson121@gmail.com>
2024-10-16 11:50:01 -0300

    added styling to homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,9 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
-      <main>
-        <div>
+    <div className="bg-black bg-home-img bg-cover bg-center">
+      <main className="flex flex-col justify-center text-center max-w-5xl mx-auto h-dvh">
+        <div className="flex flex-col hap-6 p-12 rounded-xl bg-black/90 w-4/5 sm:max-w-96 mx-auto text-white sm:text-2xl">
           <h1>Ticket System</h1>
           <address>
             777 Lucky Street
