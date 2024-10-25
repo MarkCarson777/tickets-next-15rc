92764a625dc187eb3d844bcd039d9ddadec1e61e
Mark Carson <markcarson121@gmail.com>
2024-10-25 12:23:15 -0300

    added comments to middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -10,6 +10,17 @@ export default withAuth(
   }
 );
 export const config = {
+  /*
+  	(?!...): This is a negative lookahead, meaning it ensures that the following patterns do not match.
+	  api: Excludes paths that start with /api (likely to exclude API routes).
+	  _next/static and _next/image: Excludes Next.js’ internal static and image handling routes.
+	  auth: Excludes routes related to authentication (e.g., /auth).
+	  favicon.ico and robots.txt: Excludes requests for common files like favicon.ico and robots.txt.
+	  images: Excludes paths related to image handling.
+	  login: Excludes routes related to login (e.g., /login).
+	  $: This indicates the end of the URL string.
+	  .*: Matches any other path that doesn’t match the excluded patterns.
+  */
   matcher: [
     "/((?!api|_next/static|_next/image|auth|favicon.ico|robots.txt|images|login|$).*)",
   ],
