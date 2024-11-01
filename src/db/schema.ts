bb9e71a81ef8379880014cb7f1e43573f0503764
Mark Carson <markcarson121@gmail.com>
2024-11-01 11:54:42 -0300

    added relations to the schema

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -28,3 +28,30 @@ export const customers = pgTable("customers", {
     .defaultNow()
     .$onUpdate(() => new Date()),
 });
+
+export const tickets = pgTable("tickets", {
+  id: serial("id").primaryKey(),
+  customerId: integer("customer_id")
+    .notNull()
+    .references(() => customers.id),
+  title: varchar("title").notNull(),
+  description: text("description"),
+  completed: boolean("completed").notNull().default(false),
+  tech: varchar("tech").notNull().default("unassigned"),
+  createdAt: timestamp("created_at").notNull().defaultNow(),
+  updatedAt: timestamp("updated_at")
+    .notNull()
+    .defaultNow()
+    .$onUpdate(() => new Date()),
+});
+
+export const customerRelations = relations(customers, ({ many }) => ({
+  tickets: many(tickets),
+}));
+
+export const ticketRelations = relations(tickets, ({ one }) => ({
+  customer: one(customers, {
+    fields: [tickets.customerId],
+    references: [customers.id],
+  }),
+}));
